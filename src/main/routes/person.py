f4da2f5ecb2f490f22605ec3d8faf948f373df8d
irllamartins <39415559+irllamartins@users.noreply.github.com>
2021-11-30 13:51:49 -0300

    Update person.py

--- src/main/routes/person.py
+++ src/main/routes/person.py
@@ -1,36 +1,71 @@
+import re
+from typing import Collection
+from flask import jsonify, request,Blueprint,Response
+from pymongo import TEXT
 from ...infrastructure.database.connection.mongodb import db
 from bson import ObjectId
-from flask import request
-#person_collection=db['person']
-class PersonReposity:
-    def __init__(self,person_collection):
-        self.person_collection = person_collection   
-      
-    def find_all(self):
-        search=list(self.person_collection.find())
-        return search
+import json
+from src.infrastructure.repository.person_repository import PersonReposity
+person_collection=db['person']
+PEOPLE=Blueprint('person',__name__)
 
-    def find_by_id(self,person_id):
-        if(self.person_collection.find_one({'_id':ObjectId(person_id)})==None):
-            return 
-        person=dict(self.person_collection.find_one({'_id':ObjectId(person_id)}))
+
+@PEOPLE.route('/people', methods=["GET"])
+def getAll():
+    search=PersonReposity(person_collection).find_all()
+    #print(search)
+    #search=search.find_all()
+    people=[]
+    for person in search:
         person['_id']=str(person['_id'])
-        return person
+        people.append(person)
+    return Response(json.dumps(person),status=200,content_type="application/json")
+
+@PEOPLE.route('/people/<person_id>', methods=["GET"])
+def get(person_id):
+    person=PersonReposity(person_collection).find_by_id(person_id)
+    if(person==None):
+        return Response(status=200)
+    return  Response(json.dumps(person),status=200,content_type="application/json")
+
+@PEOPLE.route('/people',methods=["POST"])  
+def post():
         
-    def create(self):
-        person=dict(name = request.json['name'],cpf = request.json['cpf'],job=request.json['job'])
-        self.person_collection.insert_one(person)
-        person['_id']=str(person['_id'])
-        return person
+    if( "cpf" in request.json and "name" in request.json and "job" in request.json):
+        """if not isinstance(request.json['id_person'], str):
+            return jsonify({"message":"id must be string"}, 400)
+        if not isinstance(request.json['cpf'], str):
+            return jsonify({"message":"cpf must be string"}, 400)
+        if not re.match("[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}$", request.json['cpf']):
+            return jsonify({"message":"cpf is invalid"},400)
+        if not isinstance(request.json['name'], str):
+            return jsonify({"message":"name must be string"}, 400)
+        if not isinstance(request.json['job'], str):
+            return jsonify({"message":"job must be integer"}, 400)
+        for person in people:
+            if person['cpf']==request.json['cpf']:
+                return jsonify({"message":"person already exists"}, 409)"""
        
-    def put(self,person_id,update):
-        if(self.person_collection.update_one({'_id':ObjectId(person_id)},{'$set':update})==None): 
-           return
-        person=dict(self.person_collection.find_one({'_id':ObjectId(person_id)}))
-        #person['_id']=str(person['_id'])
-        return person
+        person=PersonReposity(person_collection).create()
+        #print(person)
+        return Response(json.dumps(person),status=201,content_type="application/json")
+    return {"message":"must have name,cpf and job."}, 400
 
-    def delete_by_id(self,person_id):
-        person= self.person_collection.delete_one({'_id':ObjectId(person_id)})
-        return person.deleted_count
-    
+@PEOPLE.route('/people/<person_id>',methods=["PUT"])
+def put(person_id):
+    update={}
+    if "job" in request.json:
+        update['job'] = request.json['job']
+    if "name" in request.json:
+        update['name'] = request.json['name'] 
+    person = PersonReposity(person_collection).put(person_id,update)
+    person['_id']=str(person['_id'])
+    if(person!=None):
+        return Response(json.dumps(person),status=200,content_type="application/json")
+    return {'mensage':'person not found.'},404
+
+@PEOPLE.route('/people/<person_id>',methods=["DELETE"])
+def delete(person_id):
+    if(PersonReposity(person_collection).delete_by_id(person_id)!=0):
+        return {'mensage':'person deleted'},200
+    return Response(status=200)
